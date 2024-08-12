8ad1467bf1cf527940a0b8b972ed5f56f8d8172b
Ali omidi <saeedd5@yahoo.com>
2024-08-12 15:01:20 +0330

    Create Project

--- main.py
+++ main.py
@@ -13,8 +13,17 @@ df["Date"] = pd.to_datetime(df["Date"])
 df["JalaliDate"] = df["Date"].apply(lambda x: JalaliDate(x))
 df["JalaliWeekDay"] = df["Date"].apply(lambda d: JalaliDate(d).isoweekday())
 
-sun_list = df.where(df["JalaliWeekDay"] == 1)
-wed_list = df.where(df["JalaliWeekDay"] == 5)
+sun_list = df[df["JalaliWeekDay"] == 1]
+wed_list = df[df["JalaliWeekDay"] == 5]
 
+sun_open = sun_list["Open"]
+wed_close = sun_list["Close"]
 
-print(sun_list.dropna())
+df["Benefit"] = wed_close - sun_open
+
+# print(sun_list.dropna())
+# print(sun_open)
+# print(wed_close)
+# print(df["Benefit"])
+
+print(df["Benefit"].dropna())
